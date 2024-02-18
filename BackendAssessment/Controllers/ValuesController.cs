a02de0a3a216d780379ba57d08fbefe0bf612c01
Ravidevops6060 <rsravishankarnet@gmail.com>
2024-02-18 21:50:19 +0800

    first commit

---
+++ BackendAssessment/Controllers/ValuesController.cs
@@ -0,0 +1,212 @@
+﻿using BackendAssessment.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BackendAssessment.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        SqlConnection sqlconn = new SqlConnection(ConfigurationManager.ConnectionStrings[""].ConnectionString);
+        FreeLancerEmplist FLE = new FreeLancerEmplist();
+
+        // GET api/values
+        public List<FreeLancerEmplist> Get()
+        {
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("Usp_GetAllEmpList", sqlconn);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                List<FreeLancerEmplist> lstfreelanceremplist = new List<FreeLancerEmplist>();
+                if (dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dt.Rows.Count - 1; i++)
+                    {
+                        FreeLancerEmplist FLE = new FreeLancerEmplist();
+                        FLE.UserName = dt.Rows[i]["UserName"].ToString();
+                        FLE.Mail = dt.Rows[i]["Mail"].ToString();
+                        FLE.PhoneNumber = Convert.ToInt16(dt.Rows[i]["PhoneNumber"]);
+                        FLE.SkillSets = dt.Rows[i]["SkillSets"].ToString();
+                        FLE.Hobby = dt.Rows[i]["Hobby"].ToString();
+                        lstfreelanceremplist.Add(FLE);
+                    }
+                }
+                if (lstfreelanceremplist.Count > 0)
+                {
+                    return lstfreelanceremplist;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("error- " + ex.Message);
+            }
+            
+        }
+
+        // GET api/values/5
+        public FreeLancerEmplist Get(int id)
+        {
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter("Usp_GetEmpListbyID", sqlconn);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.SelectCommand.Parameters.AddWithValue("@ID", id);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                FreeLancerEmplist FLE = new FreeLancerEmplist();
+                 if (dt.Rows.Count > 0)
+                {
+                   
+                        
+                        FLE.UserName = dt.Rows[0]["UserName"].ToString();
+                        FLE.Mail = dt.Rows[0]["Mail"].ToString();
+                        FLE.PhoneNumber = Convert.ToInt16(dt.Rows[0]["PhoneNumber"]);
+                        FLE.SkillSets = dt.Rows[0]["SkillSets"].ToString();
+                        FLE.Hobby = dt.Rows[0]["Hobby"].ToString();
+                        
+                    
+                }
+
+                 if (FLE != null)
+                 {
+                     return FLE;
+                 }
+                 else
+                 {
+                     return null;
+                 }                
+               
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("error- " + ex.Message);
+            }
+        }
+
+        // POST api/values
+        public string Post(FreeLancerEmplist freeLancerEmplist)
+        {
+            string RetStr = "";
+            try 
+            {
+                if (freeLancerEmplist != null)
+                {
+                    SqlCommand cmd = new SqlCommand("Usp_AddEmployee", sqlconn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@UserName", freeLancerEmplist.UserName);
+                    cmd.Parameters.AddWithValue("@Mail", freeLancerEmplist.Mail);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", freeLancerEmplist.PhoneNumber);
+                    cmd.Parameters.AddWithValue("@SkillSets", freeLancerEmplist.SkillSets);
+                    cmd.Parameters.AddWithValue("@Hobby", freeLancerEmplist.Hobby);
+                    sqlconn.Open();
+                        int i = cmd.ExecuteNonQuery();
+                    sqlconn.Close();
+                    if (i > 0)
+                    {
+                        RetStr= "Data Inserted Successfully!";
+                    }
+                    else
+                    {
+                        RetStr = "error";
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("error- " + ex.Message);
+            }
+            return RetStr;
+        }
+
+        // PUT api/values/5
+        public string Put(int id, FreeLancerEmplist freeLancerEmplist)
+        {
+           
+            string RetStr = "";
+            try 
+            {
+                if (freeLancerEmplist != null)
+                {
+                    SqlCommand cmd = new SqlCommand("Usp_UpdateEmpListbyID", sqlconn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id", freeLancerEmplist.Id);
+                    cmd.Parameters.AddWithValue("@UserName", freeLancerEmplist.UserName);
+                    cmd.Parameters.AddWithValue("@Mail", freeLancerEmplist.Mail);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", freeLancerEmplist.PhoneNumber);
+                    cmd.Parameters.AddWithValue("@SkillSets", freeLancerEmplist.SkillSets);
+                    cmd.Parameters.AddWithValue("@Hobby", freeLancerEmplist.Hobby);
+                    sqlconn.Open();
+                    int i = cmd.ExecuteNonQuery();
+                    sqlconn.Close();
+
+                    if (i > 0)
+                    {
+                        RetStr= "Data Updated Successfully!";
+                    }
+                    else
+                    {
+                        RetStr = "error";
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("error- " + ex.Message);
+            }
+            return RetStr;
+
+        }
+
+        // DELETE api/values/5
+        public string Delete(int id)
+        {
+            string RetStr = "";
+            try
+            {
+                if (id>0)
+                {
+                    SqlCommand cmd = new SqlCommand("Usp_DeleteEmpListbyID", sqlconn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id", id);                   
+                    sqlconn.Open();
+                    int i = cmd.ExecuteNonQuery();
+                    sqlconn.Close();
+
+                    if (i > 0)
+                    {
+                        RetStr = "Data Deleted Successfully!";
+                    }
+                    else
+                    {
+                        RetStr = "error";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("error- " + ex.Message);
+            }
+            return RetStr;
+        }
+
+        
+    }
+}
---
+++ BackendAssessment/Models/FreeLancerEmplist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendAssessment.Models
+{
+    public class FreeLancerEmplist
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+        public string Mail { get; set; }
+        public int PhoneNumber { get; set; }
+        public string SkillSets { get; set; }
+        public string Hobby { get; set; } 
+    }
+}
